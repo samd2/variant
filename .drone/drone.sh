c43ae129cb36fb4d518cbbe171c1b31f8c239341
sdarwin <samuel.d.darwin@gmail.com>
2021-02-03 20:50:02 +0000

    add drone config

--- .drone/drone.sh
+++ .drone/drone.sh
@@ -53,7 +53,27 @@ sh -c "../../../b2 -j2 $B2_ARGS"
 
 echo '==================================> AFTER_SUCCESS'
 
-['mkdir -p $TRAVIS_BUILD_DIR/coverals', 'find ../../../bin.v2/ -name "*.gcda" -exec cp "{}" $TRAVIS_BUILD_DIR/coverals/ \\;', 'find ../../../bin.v2/ -name "*.gcno" -exec cp "{}" $TRAVIS_BUILD_DIR/coverals/ \\;', 'find ../../../bin.v2/ -name "*.da" -exec cp "{}" $TRAVIS_BUILD_DIR/coverals/ \\;', 'find ../../../bin.v2/ -name "*.no" -exec cp "{}" $TRAVIS_BUILD_DIR/coverals/ \\;', 'wget https://github.com/linux-test-project/lcov/archive/v1.14.zip', 'unzip v1.14.zip', 'LCOV="`pwd`/lcov-1.14/bin/lcov --gcov-tool $GCOVTOOL"', 'mkdir -p ~/.local/bin', 'echo -e \'#!/bin/bash\\nexec llvm-cov gcov "$@"\' > ~/.local/bin/gcov_for_clang.sh', 'chmod 755 ~/.local/bin/gcov_for_clang.sh', 'echo "$LCOV --directory $TRAVIS_BUILD_DIR/coverals --base-directory `pwd` --capture --output-file $TRAVIS_BUILD_DIR/coverals/coverage.info"', '$LCOV --directory $TRAVIS_BUILD_DIR/coverals --base-directory `pwd` --capture --output-file $TRAVIS_BUILD_DIR/coverals/coverage.info', 'cd $BOOST', '$LCOV --remove $TRAVIS_BUILD_DIR/coverals/coverage.info "/usr*" "*/$BOOST_LIBS_FOLDER/test/*" $IGNORE_COVERAGE "*/$BOOST_LIBS_FOLDER/tests/*" "*/$BOOST_LIBS_FOLDER/examples/*" "*/$BOOST_LIBS_FOLDER/example/*" -o $TRAVIS_BUILD_DIR/coverals/coverage.info', 'OTHER_LIBS=`grep "submodule .*" .gitmodules | sed \'s/\\[submodule\\ "\\(.*\\)"\\]/"\\*\\/boost\\/\\1\\.hpp" "\\*\\/boost\\/\\1\\/\\*"/g\'| sed "/\\"\\*\\/boost\\/$BOOST_LIBS_FOLDER\\/\\*\\"/d" | sed \':a;N;$!ba;s/\\n/ /g\'`', 'echo $OTHER_LIBS', 'eval "$LCOV --remove $TRAVIS_BUILD_DIR/coverals/coverage.info $OTHER_LIBS -o $TRAVIS_BUILD_DIR/coverals/coverage.info"', 'cd $TRAVIS_BUILD_DIR', 'gem install coveralls-lcov', 'coveralls-lcov coverals/coverage.info']
+mkdir -p $TRAVIS_BUILD_DIR/coverals
+find ../../../bin.v2/ -name "*.gcda" -exec cp "{}" $TRAVIS_BUILD_DIR/coverals/ \;
+find ../../../bin.v2/ -name "*.gcno" -exec cp "{}" $TRAVIS_BUILD_DIR/coverals/ \;
+find ../../../bin.v2/ -name "*.da" -exec cp "{}" $TRAVIS_BUILD_DIR/coverals/ \;
+find ../../../bin.v2/ -name "*.no" -exec cp "{}" $TRAVIS_BUILD_DIR/coverals/ \;
+wget https://github.com/linux-test-project/lcov/archive/v1.14.zip
+unzip v1.14.zip
+LCOV="`pwd`/lcov-1.14/bin/lcov --gcov-tool $GCOVTOOL"
+mkdir -p ~/.local/bin
+echo -e '#!/bin/bash\nexec llvm-cov gcov "$@"' > ~/.local/bin/gcov_for_clang.sh
+chmod 755 ~/.local/bin/gcov_for_clang.sh
+echo "$LCOV --directory $TRAVIS_BUILD_DIR/coverals --base-directory `pwd` --capture --output-file $TRAVIS_BUILD_DIR/coverals/coverage.info"
+$LCOV --directory $TRAVIS_BUILD_DIR/coverals --base-directory `pwd` --capture --output-file $TRAVIS_BUILD_DIR/coverals/coverage.info
+cd $BOOST
+$LCOV --remove $TRAVIS_BUILD_DIR/coverals/coverage.info "/usr*" "*/$BOOST_LIBS_FOLDER/test/*" $IGNORE_COVERAGE "*/$BOOST_LIBS_FOLDER/tests/*" "*/$BOOST_LIBS_FOLDER/examples/*" "*/$BOOST_LIBS_FOLDER/example/*" -o $TRAVIS_BUILD_DIR/coverals/coverage.info
+OTHER_LIBS=`grep "submodule .*" .gitmodules | sed 's/\[submodule\ "\(.*\)"\]/"\*\/boost\/\1\.hpp" "\*\/boost\/\1\/\*"/g'| sed "/\"\*\/boost\/$BOOST_LIBS_FOLDER\/\*\"/d" | sed ':a;N;$!ba;s/\n/ /g'`
+echo $OTHER_LIBS
+eval "$LCOV --remove $TRAVIS_BUILD_DIR/coverals/coverage.info $OTHER_LIBS -o $TRAVIS_BUILD_DIR/coverals/coverage.info"
+cd $TRAVIS_BUILD_DIR
+gem install coveralls-lcov
+coveralls-lcov coverals/coverage.info
 
 
 fi
